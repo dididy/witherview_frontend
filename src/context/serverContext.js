ae319ff8267cac2cc0e1580eaf0def5abb28a118
YongjaeLee <dev.yongjaelee@gmail.com>
2021-04-10 23:51:01 +0900

    refactor: 💡 accessToken이 null이 아닌 경우에만 Authorization을 포함한다
    - accessToken이 존재할 경우에만 headers 객체에 Authorization 추가

--- src/context/serverContext.js
+++ src/context/serverContext.js
@@ -2,23 +2,33 @@ import Axios from 'axios';
 
 const SERVER_URL = process.env.REACT_APP_API_SERVER_URL || 'http://15.164.232.147:8080';
 
-const accessToken = sessionStorage.getItem('accessToken');
-
 const api = ({
-  url, type = 'get', param, contentType = 'application/json',
-}) => Axios({
-  method: type,
-  url: `${SERVER_URL}${url}`,
-  data: param,
-  headers: {
-    Authorization: accessToken && `Bearer ${accessToken}`,
+  url,
+  type = 'get',
+  param,
+  contentType = 'application/json',
+}) => {
+  const accessToken = sessionStorage.getItem('accessToken');
+
+  const headers = {
     'Content-Type': contentType,
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
     'Access-Control-Allow-Headers':
-        'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
-  },
-  withCredentials: true,
-});
+      'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
+  };
+
+  if (accessToken !== null) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return Axios({
+    method: type,
+    url: `${SERVER_URL}${url}`,
+    headers,
+    data: param,
+    withCredentials: true,
+  });
+};
 
 export default api;
